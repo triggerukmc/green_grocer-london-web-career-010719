81381dc927027ebe935950bae10b0d821258da00
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-06 19:49:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,64 @@
 def consolidate_cart(cart)
-  # code here
-end
+  # code here	  consolidated_cart = {}
+  cart.each do |item|
+    item_name = item.keys[0]
+    consolidated_cart[item_name] = item.values[0]
+    if consolidated_cart[item_name][:count]
+      consolidated_cart[item_name][:count] += 1
+    else
+      consolidated_cart[item_name][:count] = 1
+    end
+  end
+   consolidated_cart
+end	end
 
-def apply_coupons(cart, coupons)
-  # code here
-end
 
-def apply_clearance(cart)
-  # code here
-end
+ def apply_coupons(cart, coupons)	def apply_coupons(cart, coupons)
+  # code here	  # code here
+  coupons.each do |coupon|
+    coupon_name = coupon[:item]
+    coupon_item_num = coupon[:num]
+    cart_item = cart[coupon_name]
+    next if cart_item.nil? || cart_item[:count] < coupon_item_num
+      cart_item[:count] -= coupon_item_num
+    coupon_in_cart = cart["#{coupon_name} W/COUPON"]
+    if c  oupon_in_cart
+      coupon_in_cart[:count] += 1
+    else
+      cart["#{coupon_name} W/COUPON"] = {
+          price: coupon[:cost],
+          clearance: cart_item[:clearance],
+          count: 1
+        }
+    end
+  end
+  cart
+end	end
 
-def checkout(cart, coupons)
-  # code here
-end
+
+ def apply_clearance(cart)	def apply_clearance(cart:[])
+  # code here	  # code here
+  cart.each do |item_name, item_data|
+    if item_data[:clearance]
+      item_data[:price] = (item_data[:price] * 0.8).round(1)
+    end
+  end
+end	end
+
+
+ def checkout(cart, coupons)	def checkout(cart: [], coupons: [])
+  # code here	  # code here
+  cart = consolidate_cart(cart: cart)
+  cart = apply_coupons(cart: cart, coupons: coupons)
+  cart = apply_clearance(cart: cart)
+  total = 0
+  cart.each do |item_name, item_data|
+    total += (item_data[:price] * item_data[:count])
+  end
+
+   if total > 100
+    0.9 * total
+  else
+    total
+  end
+end	end
